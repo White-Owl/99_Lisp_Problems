1634f9736416250d3eaab5dd22bfd19029d6ede9
White-Owl <White-Owl@users.noreply.github.com>
2017-09-13 15:28:20 -0400

    p07

---
+++ p07.lisp
@@ -0,0 +1,20 @@
+;;; Flatten a nested list structure.
+;;;    Transform a list, possibly holding lists as elements into a `flat' list by replacing each list with its elements (recursively).
+;;;
+;;;    Example:
+;;;    * (my-flatten '(a (b (c d) e)))
+;;;    (A B C D E)
+;;;
+;;;    Hint: Use the predefined functions list and append.
+
+(defun my-flatten (lst)
+	(when lst
+		(if (atom lst)
+			(list lst)
+			(if (atom (car lst))
+        		(append (list (car lst)) (my-flatten (cdr lst)))
+	    	    (append (my-flatten (car lst)) (my-flatten (cdr lst)))
+			)
+		)
+	)
+)
